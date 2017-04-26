2750ce2e2911cfa4dc50f19976d42f5e06e1c14a
a-gradim <andregradim@gmail.com>
2017-04-26 09:45:27 +0100

    Initial commit

---
+++ hartimu.h
@@ -0,0 +1,35 @@
+#ifndef _HARTIMU_H
+#define _HARTIMU_H
+
+#define MPU_addr 0x68
+
+      void calibrate_mpu();
+      /*!< Function that calibrates the IMU.
+           Saves 4000 values and takes the mean value from them.
+           This mean values should be used to correct offsets that accumulate during usage*/
+
+      void initialize_imu();
+      /*!< First fuction to be used on code, usually on the setup() function
+           It starts the IMU I2C connection and calibrates*/
+
+      void read_mpu_values();
+      /*!< Function used to read raw values from the IMU, raw values are
+      stored internally and cant be accessed*/
+
+      void compute_data();
+      /*!< Function that takes raw values and computes them.
+           This function calculates the X,Y and Z angles and stores them internally*/
+      float get_roll();
+      /*!< Function that returns the X angle*/
+
+      float get_pitch();
+      /*!< Function that returns the Y angle*/
+
+      float get_yaw();
+      /*!< Function that returns the Z angle*/
+
+      void start_mpu_6050();
+      /*!< Function that starts the IMU I2C connection*/
+
+
+#endif
---
+++ mainCode.ino
@@ -0,0 +1,87 @@
+#include <RingBuf.h>
+
+#include <TimerOne.h>
+#include "hartimu.h"
+#include <Wire.h>
+
+const int led = 13;  // the pin with a LED
+
+RingBuf *bufMPU = RingBuf_new(sizeof(short), 21);
+
+short teste=-741; 
+
+void setup(void){
+  //noInterrupts();
+  pinMode(led, OUTPUT);
+  digitalWrite(led,LOW);
+
+
+  initialize_imu();
+  Serial.begin(115200);
+  //Serial.println("Hello im working");
+
+  
+
+
+  Timer1.initialize(1000);
+
+  Timer1.attachInterrupt(getSensors); // blinkLED to run every 0.15 seconds
+  Timer1.start();
+}
+
+int flag=0;
+
+int countMPU=0;
+short temp;
+
+void getSensors(void){
+  digitalWrite(led, HIGH);
+  //Serial.write('i');
+  
+  countMPU+=1; 
+  if(countMPU==4){
+    
+    countMPU=0;
+    read_mpu_values();
+    compute_data();
+    temp= (short) get_yaw()*10;
+//    bufMPU->RingBufAdd(bufMPU, &temp);
+//    temp= (short) get_roll()*10;
+//    bufMPU->add(bufMPU, &temp);
+//    temp= (short) get_pitch();
+    
+//    bufMPU->add(bufMPU, &temp);
+
+
+    
+  }
+         
+  digitalWrite(led, LOW);
+  
+  
+}
+
+union sendMPU{
+  short send1;
+  byte send2[2];
+}sendMPU;
+
+void loop(void)
+{
+//  Serial.println(get_roll());
+//  if(bufMPU->numElements(bufMPU) >3){
+//  
+    bufMPU->add(bufMPU, &teste);
+    bufMPU->pull(bufMPU, &sendMPU);
+    
+    Serial.write(sendMPU.send2[0]);
+    Serial.write(sendMPU.send2[1]);
+
+//  }
+
+  
+  
+}
+
+
+
