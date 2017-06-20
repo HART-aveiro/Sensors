f88d0eb07320f4b13b93eeaf609a9619e0ac4166
¨joao.pandeirada¨ <joao.pandeirada@ua.pt>
2017-06-20 01:16:20 +0100

    clean up

--- brsh_example/brsh.cpp
+++ brsh_example/brsh.cpp
@@ -3,48 +3,32 @@
 #include <PWMServo.h>
 #include "Arduino.h"
 
-//using namespace std;
-
-
 
 void initBrush(int pin,PWMServo brushless){
-	//Servo brushless; // inicializa o objecto servo como variavel global
-	// associa a variavel servo a um pin do arduino
-	brushless.attach(pin,1000,2000);
+	
+	brushless.attach(pin,1000,2000); // associa a variavel servo a um pin do arduino
 }
 
 
 void turnOn(PWMServo brushless){
-	//liga o brushless e aguarda até ser estavel
-	//brushless.writeMicroseconds(0);
-	//brushless.writeMicroseconds(1000);
-  //brushless.write(0);
   brushless.write(0);
-	delay(5000);
+  delay(5000);
 }
 
 void turnOff(PWMServo brushless){
-	//desliga o brushless e aguarda até estar estavel
-	//brushless.writeMicroseconds(1000);
-	//brushless.writeMicroseconds(0);
-  //brushless.write(0);
   brushless.write(0);
-	delay(5000);
+  delay(5000);
 }
 
 void defineVelocity(int velocity,PWMServo brushless){
-	//dá uma velocidade e espera que estabilize, valor de velocity entre 0 e 650(reduzido para 100)
 	if (velocity<0)
 		velocity=0;
 	if (velocity>100)
 		velocity=100;
-
-	velocity=1100+velocity; // soma 1100 à velocidade para ser enviado directamente para o PWM da função SERVO
-  velocity=map(velocity,1000,2000,0,180);
- //brushless.writeMicroseconds(velocity);
+ 
+ velocity=1100+velocity; // soma 1100 à velocidade para ser enviado directamente para o PWM da função SERVO
+ velocity=map(velocity,1000,2000,0,180);
  brushless.write(velocity);
-  
-	//delay(4000);
 }
 
 
@@ -70,7 +54,7 @@ void defineVelocity(int velocity,PWMServo brushless){
   delay(10000);
 
   
-}//*/
+}*/
 
 void detach(PWMServo brushless){
 	brushless.detach();
